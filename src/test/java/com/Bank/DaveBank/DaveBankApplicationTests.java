f6a2cf7f525804cf605cdfe455a48155213b48f6
Peter David <Princewill016@gmail.com>
2024-06-05 14:27:10 +0100

    building an app to mimic traditional bank operations

---
+++ src/test/java/com/Bank/DaveBank/DaveBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bank.DaveBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaveBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
